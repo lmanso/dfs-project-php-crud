ee9a06a80a6f1b40b4908bf846b735f871584fed
lmanso <30408665+lmanso@users.noreply.github.com>
2019-07-04 11:16:55 +0200

    fix login form

--- public/login.php
+++ public/login.php
@@ -4,8 +4,8 @@
     header('Location: /articles'); ?>
 <?php } else { ?>
     <div class="container">
-        <form class="form-signin login">
-            <img class="mb-6 center" src="public/img/logo.png" alt="" width="102" height="102">
+        <form class="form-signin login" action="/loginAction" method="post">
+            <img class="mb-6 center" src="public/img/logo.png" alt="" width="102" height="102" >
             <h1 class="h3 mb-3 font-weight-normal">Connecte toi !</h1>
             <label for="username" class="sr-only">Pseudo</label>
             <input type="input" name="username" class="form-control" placeholder="Pseudo" required autofocus>
